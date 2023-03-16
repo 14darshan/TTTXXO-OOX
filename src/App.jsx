10f1e63e9114e2f0f5a720de30e688b5b678f380
nahsrads <50013166+14darshan@users.noreply.github.com>
2023-03-16 17:02:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import React, { useState,useEffect } from "react";
+import { Board } from "./components/board";
+import { ResetButton } from "./components/resetButton";
+import { ScoreBoard } from "./components/scoreboard";
+import './App.css';
+
+const App = () => {
+ 
+  function handleCallbackResponse(response){
+  console.log("encoded:" +response.credential);
+
+  } 
+
+  useEffect(() => {
+    /**global google */
+    google.accounts.id.initialize({
+      client_id: "225789599339-md035hd93no57srq0lsc0lhdq2q7nfnm.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+  google.accounts.id.renderButton(
+    document.getElementById("signInDiv"),
+    { type: 'standard',
+    size: 'large',
+    text: 'signin_with',
+    shape: 'rectangular',
+    logo_alignment: 'center',
+    width: 250}
+  );
+
+
+  }, []);
+
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  const [xPlaying, setXPlaying] = useState(true);
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [scores, setScores] = useState({ xScore: 0, oScore: 0 })
+  const [gameOver, setGameOver] = useState(false);
+
+  const handleBoxClick = (boxIdx) => {
+    // Step 1: Update the board
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying ? "X" : "O";
+      } else {
+        return value;
+      }
+    })
+
+    setBoard(updatedBoard);
+
+    // Step 2: Check if either player has won the game
+    const winner = checkWinner(updatedBoard);
+
+    if (winner) {
+      if (winner === "O") {
+        let { oScore } = scores;
+        oScore += 1;
+        setScores({ ...scores, oScore })
+      } else {
+        let { xScore } = scores;
+        xScore += 1;
+        setScores({ ...scores, xScore })
+      }
+    }
+
+    // Step 3: Change active player
+    setXPlaying(!xPlaying);
+  }
+
+  const checkWinner = (board) => {
+    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const [x, y, z] = WIN_CONDITIONS[i];
+
+
+      if ((board[x] === 'X' && board[y] === 'X' && board[z] === 'O') || (board[x] === 'O' && board[y] === 'O' && board[z] === 'X')) {
+  setGameOver(true);
+  return board[x];
+}
+      // // Iterate through win conditions and check if either player satisfies them
+      // if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+      //   setGameOver(true);
+      //   return board[x];
+      // }
+      
+
+    }
+  }
+
+  const resetBoard = () => {
+    setGameOver(false);
+    setBoard(Array(9).fill(null));
+  }
+
+  return (
+    <div className="App">
+      <ScoreBoard scores={scores} xPlaying={xPlaying} />
+      <Board board={board} onClick={gameOver ? resetBoard : handleBoxClick} />
+      <ResetButton resetBoard={resetBoard} />
+    </div>
+  );
+}
+
+export default App;
